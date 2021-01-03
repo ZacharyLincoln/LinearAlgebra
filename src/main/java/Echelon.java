f900942443f666e1916998b7041cd7aac27c9935
Zachary <zlincoln365@gmail.com>
2021-01-03 13:06:14 -0800

    Fixed Echelon.gaussianElimination to account for infinite solutions and no solutions

--- src/main/java/Echelon.java
+++ src/main/java/Echelon.java
@@ -1,8 +1,12 @@
+import java.util.ArrayList;
+import java.util.List;
+
 public class Echelon {
 
     public void gaussianElimination(Matrix m){
         RowOperations rowOperations = new RowOperations();
         double[][] matrix = m.getMatrix();
+        List<String> operations = new ArrayList<>();
 
         for(int i = 0; i < matrix.length; i++){
 
@@ -11,22 +15,37 @@ public class Echelon {
 
             //TODO Need to verify this works.
             //If row i column i is a 0, swap out for a row below.
-            while (pivot == 0){
-                rowOperations.switching(m,i,i++);
-                pivot = m.getMatrix()[i][i];
+            String step = "";
+            if(i == matrix.length - 1 && pivot == 0) {
+                return;
+            }else{}
+            while (pivot == 0) {
+                step = "R" + i + " <--> R" + (i + 1);
+                rowOperations.switching(m, temp, (i + 1));
+                //System.out.println("Pivot: "+pivot);
+                pivot = m.getMatrix()[temp][temp];
+                i++;
+            }
+
+            if (step.equals("")) {
+
+            } else {
+                operations.add(step);
             }
             i = temp;
 
             //Get row i and column i to 1
-            rowOperations.scaling(m,i,1/pivot);
+            operations.add(1 / pivot + "(R" + i + ") -> R" + i);
+            rowOperations.scaling(m, i, 1 / pivot);
 
             //Get all other row's column i to 0
             //j is row i is column
-            for(int j = 0; j < matrix.length; j++){
+            for (int j = 0; j < matrix.length; j++) {
                 //Make sure you don't make row i(j in this case) column i a zero
-                if(j == i){
-                }else {
-                    rowOperations.addingScaled(m,i,j,(-1*matrix[j][i]));
+                if (j == i) {
+                } else {
+                    operations.add((-1 * matrix[j][i]) + "(R" + i + ") + R" + j + " -> R" + j);
+                    rowOperations.addingScaled(m, i, j, (-1 * matrix[j][i]));
                 }
             }
         }
@@ -39,6 +58,11 @@ public class Echelon {
                 }
             }
         }
+
+        for(String s : operations){
+            System.out.println(s);
+        }
+
     }
 
 }
--- src/main/java/tester.java
+++ src/main/java/tester.java
@@ -3,16 +3,28 @@ public class tester {
     public static void main(String[] args){
 
         double[][] matrix = {
-                {3,-2,1,2},
-                {-1,-5,4,1},
-                {1,4,-6,-9}
+                {1,0,4,11},{1,-1,4,6},{2,0,9,25}
         };
 
-        Matrix matrix1 = new Matrix(matrix);
-        Echelon echelon = new Echelon();
-        System.out.println(matrix1);
-        echelon.gaussianElimination(matrix1);
-        System.out.println(matrix1);
-    }
+        double[][] matrix1 = {
+                {1,-1,2,-3},{4,4,-2,1},{-2,2,-4,6}
+        };
 
+        double[][] matrix2 = {
+                {-1,-2,3,-30},{-2,-3,-5,22},{1,5,5,-11}
+        };
+
+        double[][] matrix3 = {
+                {3,9,-3,24},{1,-3,11,-2},{-2,5,-20,-5}
+        };
+
+        double[][] matrix4 = {
+                {1,2,-3,7,4},{3,-1,-2,0,12},{-2,5,0,2,5},{2,3,-5,11,8}
+        };
+        Matrix m = new Matrix(matrix4);
+        Echelon echelon = new Echelon();
+        System.out.println(m);
+        echelon.gaussianElimination(m);
+        System.out.println(m);
+    }
 }
